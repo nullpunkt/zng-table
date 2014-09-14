b7221c6ac8a66cd3293afb3b8cf682e9f3998176
nullpunkt <tobias.neeb@gmail.com>
2014-09-14 12:12:39 +0200

    another commit, nothing to use yet

--- demo/js/main.js
+++ demo/js/main.js
@@ -1,1 +1,51 @@
-var app = angular.module('main', ['zng.table']);
+window.zi = 0;
+var app = angular.module('demo', ['zng.table']);
+app.controller('DemoCtrl', function($scope, zngTable){
+
+    $scope.newObjName = "";
+
+    $scope.objData = function(){
+        var names = ["Horst", "Hans", "Vivi", "Kate", "Klaas", "Carol", "Peet"],
+            data = [];
+        angular.forEach(names, function(name) { data.push(objData(name)) });
+        return data;
+    }();
+
+    $scope.objTable = zngTable.create({
+        css: {
+            table: [
+                'table',
+                'table-striped'
+            ]
+        }
+    })
+        .addField("Name", "name")
+        .addField("Mail", "mail")
+        .addField("Web", "web")
+        .addField("Visits", "visits")
+        .setData($scope.objData)
+    ;
+//    {
+//        styles: {
+//            clazz: [
+//                'table',
+//                'table-striped'
+//            ]
+//        }
+//    }
+    $scope.addObjData = function() {
+        $scope.objData.push(objData($scope.newObjName));
+        $scope.objTable.setData($scope.objData);
+        $scope.newObjName = "";
+    };
+    
+    function objData(name) {
+        return {
+            id: ++window.zi,
+            name: name,
+            mail: name.toLowerCase()+"@mail.com",
+            web: "http://"+name.toLowerCase()+".mywebsite.com",
+            visits: Math.abs(Math.floor(Math.sin(window.zi)*100))
+        };
+    };
+});
--- src/js/zng-table.js
+++ src/js/zng-table.js
@@ -8,48 +8,109 @@ table.directive('zngTable', function() {
         replace: true,
         templateUrl: '/template/zng-table/table.html',
         scope: {
+            config: "="
         },
         controller: function($scope) {
-            $scope.columns = [
-                {
-                    value: "topic1",
-                    css: "topic1"
+
+            $scope.columns = {};
+            $scope.rows = {};
+
+            $scope.$watch('config', function(cfg) {
+                update();
+            }, true);
+            function update() {
+                var columns = [],
+                    rows = [],
+                    fields = $scope.config.fields,
+                    data = $scope.config.data;
+                angular.forEach(fields, function(field) {
+                    columns.push(field.toHeading());
+                });
+                angular.forEach(data, function(rowData) {
+                    var row = {
+                        id: null,
+                        fields: []
+                    };
+                    angular.forEach(fields, function(field) {
+                        row.fields.push(field.toCell(rowData));
+                    });
+                    rows.push(row);
+                });
+                $scope.columns = columns;
+                $scope.rows = rows;
+            }
+        }
+    };
+});
+
+table.directive('zngTablePagination', function() {
+    return {
+        restrict: 'E',
+        replace: true,
+        templateUrl: function(element, attrs) {
+            var template = (typeof(attrs.template)!=='undefined') ? attrs.template : '/template/zng-table/pagination.html';
+            return template;
+        },
+        scope: {
+            config: "="
+        },
+        controller: function($scope) {
+        }
+    }
+});
+
+table.service('zngTable', function() {
+    return {
+        create: function(settings) {
+            console.log(['hi', settings]);
+            var that = this;
+            if(!angular.isObject(settings)) {
+                settings = {};
+            }
+            return function() {
+                return {
+                    settings: settings,
+                    fields: [],
+                    data: [],
+                    addField: function(caption, index) {
+                        this.fields.push(that.field(caption, index));
+                        return this;
+                    },
+                    setData: function(data) {
+                        this.data = data;
+                        return this;
+                    }
+                };
+                return ret;
+            }();
+        },
+
+        field: function(caption, index) {
+
+            var _caption = caption;
+            var _index = index;
+
+            return {
+                caption: function() {
+                    return _caption;
                 },
-                {
-                    value: "topic2",
-                    css: "topic2"
+                index: function() {
+                    return _index;
                 },
-                {
-                    value: "topic3",
-                    css: "topic3"
+                toHeading: function() {
+                    return {
+                        value: _caption,
+                        class: {}
+                    }
+                },
+                toCell: function(rowData) {
+                    return {
+                        value: rowData[_index],
+                        class: {}
+                    };
                 }
-            ];
-            
-            $scope.rows = [
-                {
-                    id: 0,
-                    fields: [
-                        {
-                            value: 'bla1',
-                            class: {
-                                horst1: true
-                            }
-                        },
-                        {
-                            value: 'bla2',
-                            class: {
-                                horst1: true
-                            }
-                        },
-                        {
-                            value: 'bla3',
-                            class: {
-                                horst1: false
-                            }
-                        }
-                    ]
-                }
-            ];
+
+            }
         }
     };
 });
