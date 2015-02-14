b92dd4604a8310363f0fd039e64a8f59c1be09c1
Tobias Seipke <tobias.seipke@dericon.de>
2015-02-14 13:29:53 +0100

    Added the possibility to load external templates for table and pagination

--- src/js/zng-table.js
+++ src/js/zng-table.js
@@ -14,9 +14,11 @@ table.directive('zngTable', function() {
     return {
         restrict: 'E',
         replace: true,
-        templateUrl: '/template/zng-table/table.html',
+        templateUrl: function(elem, attrs) {
+            return attrs.template || '/template/zng-table/table.html'
+        },
         scope: {
-            table: "=config"
+            table: "=table"
         },
         
         link: function(scope, element) {
@@ -26,36 +28,36 @@ table.directive('zngTable', function() {
         },
         
         controller: function($scope) {
-            $scope.$watch('table', function() {
-//                console.log("detected new table");
+            $scope.$watch('table', function(table) {
+                //console.log("detected new table");
                 $scope.config = $scope.table.config;
             });
             $scope.$watch('table.handler.oos', function(oos) {
-                if(!oos) return;
 //                console.log("detected out of sync");
                 $scope.table.handler.update();
             });
             $scope.$watch('table.pagination', function(pagination) {
+                if(typeof pagination === 'undefined')return;
 //                console.log("detected pagination");
                 $scope.table.handler.update();
             }, true);
             $scope.$watch('__width', function(width) {
 //                console.log(width);
             });
-            
+
             $scope.sort = function(index) {
                 if(!$scope.table.handler.fields[index].sortable)return;
                 $scope.config.sort = {
-                    direction: (index===$scope.config.sort.index) 
-                        ? ($scope.config.sort.direction===zng.table.ORDER_DIRECTION_ASC) 
-                            ? zng.table.ORDER_DIRECTION_DESC 
-                            : zng.table.ORDER_DIRECTION_ASC
+                    direction: (index===$scope.config.sort.index)
+                        ? ($scope.config.sort.direction===zng.table.ORDER_DIRECTION_ASC)
+                        ? zng.table.ORDER_DIRECTION_DESC
+                        : zng.table.ORDER_DIRECTION_ASC
                         : zng.table.ORDER_DIRECTION_ASC,
                     index: index
                 };
                 $scope.table.handler.update();
             };
-            
+
             $scope.styles =  {
                 table: function() {
                     var ret = {};
@@ -87,8 +89,7 @@ table.service('zngTable', function() {
                 pagination: {
                 }
             }, cfg);
-            
-            
+
             var ret = {
                 config: cfg,
                 
@@ -159,8 +160,7 @@ zng.table.handler = {
         if(typeof zngTable === 'undefined') {
             throw 'Please insert zngTable as first argument in handler function.';
         }
-        
-        this.zngTable = zngTable;
+
         this.oos = true;
 
         // abstract
@@ -199,7 +199,7 @@ zng.table.handler = {
         
         this.setBase = function(base) {
             this.base = base;
-            this.zngTable.pagination.max = this.base.length;
+            zngTable.pagination.max = this.base.length;
             this.setOutOfSync();
         };
 
@@ -215,8 +215,6 @@ zng.table.handler = {
                 sortIndex: null,
                 clazz: []
             }, options);
-            
-            
             this.fields.push({
                 topic: topic,
                 index: index,
@@ -232,7 +230,7 @@ zng.table.handler = {
         };
                 
         this.getHeadline = function() {
-            var sort = this.zngTable.config.sort;
+            var sort = zngTable.config.sort;
             var ret = {
                 fields: []
             };
@@ -266,8 +264,8 @@ zng.table.handler = {
             
             var ret = [],
                 that = this,
-                pagination = this.zngTable.pagination,
-                sort = this.zngTable.config.sort;
+                pagination = zngTable.pagination,
+                sort = zngTable.config.sort;
         
             angular.forEach(this.base, function(row) {
                 var tmp = {
@@ -302,7 +300,9 @@ table.directive('zngPagination', function() {
     return {
         restrict: 'E',
         replace: true,
-        templateUrl: '/template/zng-table/pagination.html',
+        templateUrl: function(elem, attrs) {
+            return attrs.template || '/template/zng-table/pagination.html'
+        },
         scope: {
             pagination: "=config"
         },
