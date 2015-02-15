89a4d250a125d1252fd3ff5514272504268fa1d8
Tobias Seipke <tobias.seipke@dericon.de>
2015-02-15 18:37:57 +0100

    another bÄM!

--- jsfiddle/index.php
+++ jsfiddle/index.php
@@ -3,18 +3,16 @@ require_once 'vendor/autoload.php';
 
 $demo = 'first_table';
 $config = file_get_contents('demos/'.$demo.'/demo.details');
-$start = '- ';
-$end = '---';
+$start = 'resources:';
+
+$assets = preg_grep("/^[ ]*-(.*)$/", explode("\n", substr($config, strpos($config, $start))));
 
 $replace = array(
     'https://rawgit.com/nullpunkt/zng-table/master/src/js/zng-table.js' => '/src/js/zng-table.js'
 );
 
-
-$config = substr($config, strpos($config, $start, strpos($config, 'resources: '))+strlen($start), strrpos($config, $end)-strpos($config, $start, strpos($config, 'resources: '))+strlen($start)-strlen($end)-strlen($start));
-$assets = explode('- ', $config);
 foreach($assets as &$entry) {
-    $entry = trim($entry);
+    $entry = trim(str_replace(" - ", "", $entry));
     if(array_key_exists($entry, $replace)) {
         $entry = $replace[$entry];
     }
